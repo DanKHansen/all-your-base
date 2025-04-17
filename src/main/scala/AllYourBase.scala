d8ccc869d3cb86d991bed33f1f964df880c37972
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-17 14:06:09 +0200

    in progress

--- src/main/scala/AllYourBase.scala
+++ src/main/scala/AllYourBase.scala
@@ -1,10 +1,10 @@
 object AllYourBase:
    def rebase(fromBase: Int, number: List[Int], toBase: Int): Option[List[Int]] =
+      def asDec =
+         (for c <- (for n <- number.zip(number.indices.reverse)
+                   yield n._1 * math.pow(fromBase, n._2).toInt).sum.toString
+         yield c.toString.toInt).toList
+
       if fromBase <= 1 || toBase <= 1 || number.exists(_ < 0) || number.exists(_ >= fromBase) then None
       else if number.isEmpty then Some(List(0))
-      else
-         val decimal =
-            for n <- number.zip(number.indices.reverse)
-            yield n._1 * math.pow(fromBase, n._2).toInt
-         val result = for c <- decimal.sum.toString yield c.toString.toInt
-         Some(result.toList)
+      else Some(asDec)
