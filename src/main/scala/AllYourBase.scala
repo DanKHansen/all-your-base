f90b4155a71bf156287d089a6d1d5e0a5d839064
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-17 15:44:28 +0200

    refactor

--- src/main/scala/AllYourBase.scala
+++ src/main/scala/AllYourBase.scala
@@ -1,20 +1,19 @@
 import scala.annotation.tailrec
 
 object AllYourBase:
-   // @tailrec
    def rebase(fromBase: Int, number: List[Int], toBase: Int): Option[List[Int]] =
-      def toDec =
-         (for c <- (for n <- number.zip(number.indices.reverse)
-                   yield n._1 * math.pow(fromBase, n._2).toInt).sum.toString
-         yield c.toString.toInt).toList
-
-      @tailrec
-      def fromDec(n: List[Int], rem: List[Int]): List[Int] =
-         val q = n.mkString.toInt / toBase
-         val r = n.mkString.toInt % toBase
-         if q == 0 then r :: rem else fromDec(List(q), r :: rem)
-
-      if fromBase <= 1 || toBase <= 1 || number.exists(_ < 0) || number.exists(_ >= fromBase) then None
+      if fromBase <= 1 || toBase <= 1 || number.exists(n => n < 0 || n >= fromBase) then None
       else if number.isEmpty then Some(List(0))
-      else if toBase == 10 then Some(toDec)
-      else Some(fromDec(toDec, Nil))
+      else
+         val decimalValue = number.zipWithIndex.map { case (digit, index) =>
+            digit * math.pow(fromBase, number.length - 1 - index).toInt
+         }.sum
+
+         @tailrec
+         def fromDec(n: Int, rem: List[Int]): List[Int] =
+            if n == 0 && rem.isEmpty then List(0)
+            else if n == 0 then rem
+            else fromDec(n / toBase, (n % toBase) :: rem)
+
+         if toBase == 10 then Some(decimalValue.toString.map(_.asDigit).toList)
+         else Some(fromDec(decimalValue, Nil))
